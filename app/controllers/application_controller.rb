ff34f4d7ee9b02829ce9cc732b9aafefd4234e0e
brateq <brateq@gmail.com>
2018-09-24 11:35:52 +0200

    Set global search variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_global_search_variable
+
+  def set_global_search_variable
+    @q = Pin.ransack(params[:q])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  resources :pins
+  resources :pins do
+    collection do
+      match 'search' => 'pins#search', via: %i[get post], as: :search
+    end
+  end
+
   resources :boards
   devise_for :users
   root 'home#index'
